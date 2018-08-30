e5d209a9dec705c3144820b0b93b88391c5b9a76
user <user@JBTC10108>
2018-08-30 14:42:35 +0900

    서예주

--- 자바기초실습/src/제02차시/AppMain.java
+++ 자바기초실습/src/제02차시/AppMain.java
@@ -1,6 +1,6 @@
 package 제02차시;
 
-class AppMain {
+public class AppMain {
 	public static void main(String[] args) {
 		System.out.println("난 main() 함수!");
 		System.out.println("자바 프로그램은 나로부터 시작해!");
