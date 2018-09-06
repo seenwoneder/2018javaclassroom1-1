bcfef8f26ad15e1415dda5dc49e034b33736e039
user <user@JBTC10108>
2018-09-06 15:07:12 +0900

    서예주

--- 자바기초실습/src/제04차시/실습01.java
+++ 자바기초실습/src/제04차시/실습01.java
@@ -1,9 +1,11 @@
 package 제04차시;
 
+import java.io.IOException;
+
 public class 실습01 {
 
 	public static void main(String[] args) {
-		int b;
+		int bt;
 		try {
 			bt=System.in.read();
 			System.out.println((char)bt);
--- 자바기초실습/src/제04차시/실습02.java
+++ 자바기초실습/src/제04차시/실습02.java
@@ -1,15 +1,18 @@
 package 제04차시;
 
+import java.io.IOException;
+
+
 public class 실습02 {
 
 	public static void main(String[] args) {
 		int bt;
 		try {
 			while(( bt = System.in.read()) != -1) {
-				System.out.println((char)bt);
+				System.out.print((char)bt);
 			   }
-		   }catch(IOException e) {
-		System.err.println(e);s
+		   }catch(IOException e){
+		System.err.println(e);
 	   }
 
 	}
