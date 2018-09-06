46a2789791a9c34ab85be03e1d1577ce9f14179d
user <user@JBTC10108>
2018-09-06 14:52:03 +0900

    서예주

--- 자바기초실습/src/제04차시/실습01.java
+++ 자바기초실습/src/제04차시/실습01.java
@@ -3,7 +3,13 @@ package 제04차시;
 public class 실습01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int b;
+		try {
+			bt=System.in.read();
+			System.out.println((char)bt);
+		}catch(IOException e) {
+			System.err.println(e);
+		}
 
 	}
 
--- 자바기초실습/src/제04차시/실습02.java
+++ 자바기초실습/src/제04차시/실습02.java
@@ -3,7 +3,14 @@ package 제04차시;
 public class 실습02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int bt;
+		try {
+			while(( bt = System.in.read()) != -1) {
+				System.out.println((char)bt);
+			   }
+		   }catch(IOException e) {
+		System.err.println(e);s
+	   }
 
 	}
 
