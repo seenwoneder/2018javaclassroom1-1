4b47b48e6a818910a617aa6f41d3756e1d269012
user <user@JBTC10108>
2018-09-06 15:27:57 +0900

    서예주

---
+++ 자바기초실습/src/제04차시/실습03.java
@@ -0,0 +1,17 @@
+package 제04차시;
+
+import java.util.Scanner;
+
+public class 실습03 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		float b=s.nextFloat();
+		String c=s.next();
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+
+}
