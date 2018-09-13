805c3d63f3992e514a5ee325d5b0a955259f4971
user <user@JBTC10108>
2018-09-13 11:46:17 +0900

    서예주

---
+++ 자바과제/src/종합실습과제/문제07.java
@@ -0,0 +1,24 @@
+package 종합실습과제;
+
+import java.util.Scanner;
+
+public class 문제07 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sc.useDelimiter("\r|\n|,");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.printf("두 정수 : %d %d\n", a,b);
+		System.out.printf("a AND b: %d\n", a&b);
+		System.out.printf("a OR b: %d\n", a|b);
+		System.out.printf("NOT a: %d\n", ~a);
+		System.out.printf("a XOR b: %d\n", a^b);
+		System.out.printf("a NAND b: %d\n", ~(a&b));
+		System.out.printf("a NOR b: %d", ~(a|b));
+		
+		
+	}
+
+}
