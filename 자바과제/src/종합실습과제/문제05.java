e410477dd41470bfdd9df04d8a409aa79cee56f9
user <user@JBTC10108>
2018-09-13 11:14:52 +0900

    서예주

---
+++ 자바과제/src/종합실습과제/문제05.java
@@ -0,0 +1,19 @@
+package 종합실습과제;
+
+import java.util.Scanner;
+
+public class 문제05 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a = sc.nextInt();
+		int h = a/3600;
+		int m = (a%3600)/60;
+		int s = (a%3600)%60;
+		
+		
+		System.out.printf("%02d시 %02d분 %02d초", h, m, s);
+
+	}
+
+}
