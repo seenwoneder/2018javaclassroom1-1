99143a5b65f6b8ccd073a6c4bf6516f6784c79d5
user <user@JBTC10108>
2018-09-13 10:34:32 +0900

    서예주

---
+++ 자바과제/src/종합실습과제/문제04.java
@@ -0,0 +1,12 @@
+package 종합실습과제;
+
+import java.util.Scanner;
+
+public class 문제04 {
+	public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			String a=sc.next();
+			System.out.println( (char)(a.charAt(0)-32)  );
+			sc.close();
+		}
+	}
