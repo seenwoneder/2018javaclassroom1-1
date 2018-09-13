c75f87162c7b262a21740bb8366abf0017ba94db
user <user@JBTC10108>
2018-09-13 10:43:14 +0900

    서예주

---
+++ 자바과제/src/종합실습과제/문제02.java
@@ -0,0 +1,17 @@
+package 종합실습과제;
+
+import java.util.Scanner;
+
+public class 문제02 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		sc.close();
+		System.out.printf("몫: %d\n", a/b);
+		System.out.printf("나머지: %d", a%b);
+
+	}
+
+}
