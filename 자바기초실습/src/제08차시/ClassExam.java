a5406f07687d188cd76deb9b151128d23f8927fc
user <user@JBTC10108>
2018-10-18 15:05:25 +0900

    SeoYeJu

---
+++ 자바기초실습/src/제08차시/ClassExam.java
@@ -0,0 +1,20 @@
+package 제08차시;
+
+public class ClassExam {
+
+	public static void main(String[] args) {
+		Student kim = new Student();
+		kim.name = "김현우";
+		kim.grade = 2;
+		System.out.println("학생의 이름은 " + kim.name + "이고, " + kim.grade + "학년입니다.");
+
+	}
+
+ }
+ class Student{
+	 	String name;                //이름
+	 	int grade;                  //학년
+	 	int ban;                    //반
+	 	int number;                 //번호
+	 	String telephone;           //전화번호
+ }
