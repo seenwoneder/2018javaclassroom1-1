ac16381324eacc6e6abafa98c9d7f2551053cae6
user <user@JBTC10108>
2018-10-18 14:01:09 +0900

    서예주

--- 개념이해/src/_01객체지향언어의특징_캡슐화/Main.java
+++ 개념이해/src/_01객체지향언어의특징_캡슐화/Main.java
@@ -1,15 +1,18 @@
 package _01객체지향언어의특징_캡슐화;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
+		//윈도우 만들기
 		JFrame jf = new JFrame();
-		jf.setTitle("통신과");
-		jf.setSize(300,300);
-		jf.setLocationRelativeTo(null);
-		jf.setVisible(true);
+		jf.setTitle("통신과");  //윈도우 이름
+		jf.setSize(300,300);  //윈도우 크기
+		jf.setLocationRelativeTo(null);  //?????
+		jf.setVisible(true);  //??????
 		
 		
 	}
