1e42ee4ca8e6bca0c41949b317eb9c01a29345f1
user <user@JBTC10108>
2018-10-18 15:09:12 +0900

    SeoYeJu

--- 자바기초실습/src/제08차시/ClassExam.java
+++ 자바기초실습/src/제08차시/ClassExam.java
@@ -11,10 +11,3 @@ public class ClassExam {
 	}
 
  }
- class Student{
-	 	String name;                //이름
-	 	int grade;                  //학년
-	 	int ban;                    //반
-	 	int number;                 //번호
-	 	String telephone;           //전화번호
- }
---
+++ 자바기초실습/src/제08차시/Student.java
@@ -0,0 +1,11 @@
+package 제08차시;
+
+class Student {
+	
+	String name;             // 이름
+	int grade;               // 학년
+	int ban;                 // 반
+	int number;              // 번호
+	String telephone;        // 전화번호
+
+}
