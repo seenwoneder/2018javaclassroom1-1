2a0bb62ff523ba9d1f8e846516919fc868dd784d
user <user@JBTC10108>
2018-10-18 14:11:33 +0900

    서예주

---
+++ 개념이해/src/_02객체지향언어의특징_상속/Main.java
@@ -0,0 +1,18 @@
+package _02객체지향언어의특징_상속;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame{
+
+	public static void main (String[] args) {
+		Main m=new Main();
+		m.setTitle("통신과");
+		m.setSize(500,500);
+		m.setLocationRelativeTo(null);
+		m.setVisible(true);
+		
+	}
+
+}
