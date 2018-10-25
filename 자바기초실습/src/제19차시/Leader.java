5c02f04a44b3d616ed853a9c1f1e5755b6f556b9
user <user@JBTC10108>
2018-10-25 15:46:54 +0900

    서예주

---
+++ 자바기초실습/src/제19차시/Leader.java
@@ -0,0 +1,13 @@
+package 제19차시;
+
+public class Leader {
+	String name;
+	Leader(String a, String b, boolean c) {
+		name = a;
+	}
+	
+	void isLeader() {
+		System.out.println(name + "은 학생회장이다.");
+	}
+ 
+}
